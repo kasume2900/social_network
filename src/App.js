fbcb76e287279ac5aae37552b1cf5627ce7893fb
kasume2900 <88428878+kasume2900@users.noreply.github.com>
2021-11-08 12:15:28 +0800

    1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
+
 import './App.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app_wrapper">
+        <header className="header">
+            <div className="header__logo">
+                <img src="https://lh3.googleusercontent.com/proxy/Z3JOhKkvg9EW6OOSDd98HXKnQ2yIfYmeePZreeEH3brUbBGYl3cLHfOiFFkR4cV34FILn8BscpGej5NgO4z_fslzDt_y26NjwopaKeJ7LQ8boN6uTd_J-w" alt=""/>
+            </div>
+        </header>
+        <nav className="nav">
+            <ul>
+                <li>
+                    <a href="#" className="liink">profile</a>
+                </li>
+                <li>
+                    <a href="#" className="liink">Message</a>
+                </li>
+            </ul>
+        </nav>
+        <div className="content">ssss</div>
     </div>
   );
 }
